99b7e8fcdf9389a88d4dc6e84f379caeeb63136c
menriqueandrade <56304628+menriqueandrade@users.noreply.github.com>
2023-05-08 00:32:27 -0500

    Guardar eliminar consultar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import './App.css';
 import { useFetch } from './hook/useFetch';
-function App() {
-  //utilizamos la variable del hook
-  const { data, loading, error, handleCancelRequest } = useFetch("http://localhost:8080/clientController/clientes")
+import { useState } from 'react';
 
+function App() {
+  var consultarCliente = "http://localhost:8080/clientController/clientes";
+  var consultarProducto = "http://localhost:8080/productController/productos";
+  //utilizamos la variable del hook
+  const { data, loading, error, handleCancelRequest, deleteResource, guardarCliente,productos } = useFetch(consultarCliente)
+
+  
+
+
+  
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+  const [producto, setProducto] = useState(0);
+  // console.log(data);
+
+  //funcion para eliminar
+  const handleDelete = (id) => {
+    //console.log(id);
+    console.log(id);
+    deleteResource(id)
+
+    window.location.reload();
+  }
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    const nuevoCliente = {
+      nombre, apellido, email,
+      producto: {
+        id: producto
+      }
+    };
+    guardarCliente(nuevoCliente);
+    window.location.reload();
+  };
   return (
     <>
       <h1>
@@ -23,18 +57,125 @@ function App() {
               Cargando...
             </li>
           }
-          {data && data.map((item, index) => (
-            <li key={index}>
-              {item.nombre}-
-              {item.apellido}-
-              {item.producto.nombre}-
-              {item.producto.valor}
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Producto</th>
+                <th scope="col">Accion</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {data
+                && data.map((item, index) => (
 
-            </li>
-          ))}
+                  <tr>
+                    <th key={index}>{item.id}</th>
+                    <td>{item.nombre}</td>
+                    <td>{item.apellido}</td>
+                    <td>{item.email}</td>
+                    <td>{item.producto.nombre}-{item.producto.valor}</td>
+                    <td>
+
+                      <button type="button" className="btn btn-primary" onClick={() => handleDelete(item.id)}>Eliminar</button>
+                      <button type="button" className="btn btn-primary">Editar</button>
+                    </td>
+                  </tr>
+                ))
+
+
+              }
+            </tbody>
+          </table>
         </ul>
       </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="div">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              className="form-control"
+              id="nombre"
+              placeholder="Nombre"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+            />
+            <label htmlFor="apellido">Apellido</label>
+            <input
+              type="text"
+              className="form-control"
+              id="apellido"
+              placeholder="Apellido"
+              value={apellido}
+              onChange={(e) => setApellido(e.target.value)}
+            />
+          </div>
+          <div className="div">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="div">
+            <label htmlFor="producto">Producto</label>
+            { <select
+              className="form-control"
+              id="producto"
+              value={producto}
+              onChange={(e) => setProducto(e.target.value)}
+            >
+              <option value="">Seleccione</option>
+              {productos && productos.map((item, index) => (
+                <option key={index} value={item.id}>
+                  {item.nombre}
+                </option>
+              ))}
+            </select> }
+          </div>
 
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      {/* <form>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Nombre</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Apellido</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Correo</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Producto</label>
+          <select className="form-control" id="exampleFormControlSelect1">
+            <option>Seleccione</option>
+            <option>Producto 1</option>
+            <option>Producto 2</option>
+            <option>Producto 3</option>
+            <option>Producto 4</option>
+            <option>Producto 5</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form> */}
     </>
   );
 }
--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -13,6 +13,7 @@ export function useFetch(url) {
     const [controller, setController] = useState(null);
 
     useEffect(() => {
+        getProductos();
         //para rastrear la peticion y controlarla
         const abortController = new AbortController();
         setController(abortController);
@@ -38,7 +39,67 @@ export function useFetch(url) {
             controller.abort();
             setError("Request Cancelled")
         }
-      
+
     }
-    return { data, loading, error, handleCancelRequest };
+    //borrar Cliente
+    const [response, setResponse] = useState();
+    const deleteResource = async (id) => {
+        console.log("Estoy con el id" + id)
+        try {
+            setLoading(true);
+            const res = await fetch("http://localhost:8080/clientController/cliente/" + id, {
+                method: 'DELETE'
+            })
+            const json = await res.json();
+
+            setResponse(json);
+
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(error);
+        }
+    };
+
+    //Guardar Cliente
+    const [responseSave, setResponseSave] = useState();
+    const guardarCliente = async (data) => {
+        console.log("Estoy con el id" + data)
+        try {
+            setLoading(true);
+            const res = await fetch("http://localhost:8080/clientController/guardar", {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const json = await res.json();
+            setResponseSave(json);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(error);
+        }
+    };
+    //Get productos
+    const [productos, setProductos] = useState([]);
+    const getProductos = async () => {
+        try {
+            const res = await fetch("http://localhost:8080/productController/productos");
+            const data = await res.json();
+            // console.log(data);
+            setProductos(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+
+   
+
+
+
+    return { data, loading, error, handleCancelRequest, deleteResource, guardarCliente, productos };
 }
